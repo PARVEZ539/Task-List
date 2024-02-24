de0dcd795f11672e1caebe106d4a6efbfd845d4e
PARVEZ539 <parvezahmez33@gmail.com>
2024-02-25 02:06:31 +0900

    first commit

---
+++ Task.js
@@ -0,0 +1,12 @@
+class Task{
+    constructor(title){
+        this.id = new Date().toLocaleString();
+        this.title = title;
+        this.isCompleted = false;
+    }
+}
+
+
+
+
+export default Task;
